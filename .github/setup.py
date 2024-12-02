fb99eb0803193d1765c87f8c7df0003619dde14f
ssolone <lightnimph@gmail.com>
2024-12-02 18:49:59 +0900

    Create setup.py

---
+++ .github/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='workflows',
+  version='0.0.1',
+  url='https://github.com/ssolone/1day_1coding/new/main/.github',
+  author='sora',
+  packages=fine_packages(),
+  install_requires=['pandas'],
+)
