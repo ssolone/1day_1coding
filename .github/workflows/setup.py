0a22e68cba5419baac3d283e27ddb4ff24693688
ssolone <lightnimph@gmail.com>
2024-12-02 18:24:32 +0900

    Update setup.py

--- .github/workflows/setup.py
+++ .github/workflows/setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup, find_pakages
 
 setup(
-  name='helper',
+  name='package',
   version='0.0.1',
   description='python helper fuction',
-  author='작성자',
-  author_email='작성자 이메일',
-  license='라이센스 이름',
+  author='sora',
+  author_email='lightnimpg@gmail.com',
   packages=find_packages(),
   install_requires=['pandas'],
 )
