e737948c8e41c448f4d8b9d0ac16786c10e3f1ee
ssolone <lightnimph@gmail.com>
2024-12-02 18:15:57 +0900

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,10 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = "https://www.linkedin.com/search/results/people/?currentCompany=%5B20708%5D"
-req = requests.get(URL)
-html = req.text
-soup = BeautifulSoup(html, 'html.parser')
-
-jobs_title = soup.find_all("div",{"class":"entity-result__primary-subtitle.t-14.t-black"})
-print(jobs_title)
+def print_hello_world() :
+  print("Hello world")
