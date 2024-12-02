1b6962e7958eb1757a5f648147458282e0c794e8
ssolone <lightnimph@gmail.com>
2024-12-02 18:20:46 +0900

    Create setup.py

---
+++ .github/workflows/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_pakages
+
+setup(
+  name='helper',
+  version='0.0.1',
+  description='python helper fuction',
+  author='작성자',
+  author_email='작성자 이메일',
+  license='라이센스 이름',
+  packages=find_packages(),
+  install_requires=['pandas'],
+)
