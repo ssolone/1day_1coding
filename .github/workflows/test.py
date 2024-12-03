f89d7b01052534d1b4bc061070635cb99de8ee15
ssolone <lightnimph@gmail.com>
2024-12-03 19:01:16 +0900

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,16 +1,10 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup as bs
 
+page = requests.get("https://search.naver.com/search.naver?ssc=tab.news.all&where=news&sm=tab_jum&query=AI")
+soup = bs(page.text, "html.parser")
 
-dates = pd.date_range('20130101', periods=6)
+elements = soup.select('div.news_contents a > title')
 
-df = pd.DataFrame(np.random.randn(6,4), index=dates, columns=list('ABCD'))
-
-df2 = pd.DataFrame({'A': 1.,
-                    'B': pd.Timestamp('20130102'),
-                    'C': pd.Series(1, index=list(range(4)), dtype='float32'),
-                    'D': np.array([3]*4, dtype='int32'),
-                    'E': pd.Categorical(['test', 'train', 'test', 'train']),
-                    'F': 'foo'})
-
+for index, element in enumerate(elements, 1):
+    print("{} 번째 뉴스 제목 : {}".format(index, element.text))
