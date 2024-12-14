7c35386c462dfb20b8dffa1dfda531e15d48930a
ssolone <lightnimph@gmail.com>
2024-12-14 11:42:03 +0900

    test.py 업데이트

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -1,22 +1,30 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import csv
 
-page = requests.get("https://news.naver.com/?viewType=pc")
-soup = bs(page.content, "html.parser")
+#page = requests.get("https://news.naver.com/?viewType=pc")
+#soup = bs(page.content, "html.parser")
 
-results = soup.find(id="ResultsContainer")
+query = 'Messenger'
+url = f'https://news.google.com/search?q={query}&hl=en-US&gl=US&ceid=US:en'
 
-news_title = soup.find_all("h4", {"class":"cn_title"})
-media_com = soup.find_all("div", {"class":"cn_name"})
-news_link = soup.find_all("a", {"class":"cjs_nf_a _item _cds_link _newsflash_link"})
+response = requests.get(url)
+soup = bs(response.content, 'html.parser')
+
+#news_title = soup.find_all("h4", {"class":"cn_title"})
+#media_com = soup.find_all("div", {"class":"cn_name"})
+#news_link = soup.find_all("a", {"class":"cjs_nf_a _item _cds_link _newsflash_link"})
+
+news = soup.find_all("a", {"class":"JtKRv"})
+media_com = soup.find_all("div", {"class":"vr1PYe"})
 
 data = []
 
-for i in range(len(news_title)):
+for i in range(len(news)):
   num = i+1
-  title = news_title[i].text
+  title = news[i].text
   media = media_com[i].text
-  link = "https://news.naver.com" + news_link[i].get("href")
+  link = 'https://news.google.com'+news[i].get("href")
   data.append([num, title, media, link])
 
 print(data)
