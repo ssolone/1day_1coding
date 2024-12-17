0c390f09c4c503583c9588f48102d8065baf5256
ssolone <lightnimph@gmail.com>
2024-12-17 20:16:10 +0900

    Update summary_test.py

--- .github/workflows/summary_test.py
+++ .github/workflows/summary_test.py
@@ -7,14 +7,13 @@ rss_url = "http://feeds.feedburner.com/inven"
 print(len(rss_feed.entries))
 
 for p in rss_feed.entries:
-
     url = p.link
-
-article = Article(url, language = 'ko')
-article.download()
-article.parse()
-
-NewsFeed = article.text
-NewsSum = summarize(NewsFeed)
-
-print(article.title + '\n' + NewsFeed + '\n' + NewsSum)
+    
+    article = Article(url, language = 'ko')
+    article.download()
+    article.parse()
+    
+    NewsFeed = article.text
+    NewsSum = summarize(NewsFeed)
+    
+    print(article.title + '\n' + NewsFeed + '\n' + NewsSum)
