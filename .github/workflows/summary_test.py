3be5e3ddb78dd9a1c9e08325e350e32be2e9c261
ssolone <lightnimph@gmail.com>
2024-12-20 19:09:39 +0900

    Update summary_test.py

--- .github/workflows/summary_test.py
+++ .github/workflows/summary_test.py
@@ -1,19 +1,53 @@
-import feedparser
-from newspaper import Article
-from summa.summarizer import summarize
+import nltk 
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize, sent_tokenize 
 
-rss_url = "http://feeds.feedburner.com/inven"
+# Input text - to summarize  
+text = """ """
 
-print(len(rss_feed.entries))
+# Tokenizing the text 
+stopWords = set(stopwords.words("english")) 
+words = word_tokenize(text) 
 
-for p in rss_feed.entries:
-    url = p.link
-    
-    article = Article(url, language = 'ko')
-    article.download()
-    article.parse()
-    
-    NewsFeed = article.text
-    NewsSum = summarize(NewsFeed)
-    
-    print(article.title + '\n' + NewsFeed + '\n' + NewsSum)
+# Creating a frequency table to keep the  
+# score of each word 
+
+freqTable = dict() 
+for word in words: 
+    word = word.lower() 
+    if word in stopWords: 
+        continue
+    if word in freqTable: 
+        freqTable[word] += 1
+    else: 
+        freqTable[word] = 1
+
+# Creating a dictionary to keep the score 
+# of each sentence 
+sentences = sent_tokenize(text) 
+sentenceValue = dict() 
+
+for sentence in sentences: 
+    for word, freq in freqTable.items(): 
+        if word in sentence.lower(): 
+            if sentence in sentenceValue: 
+                sentenceValue[sentence] += freq 
+            else: 
+                sentenceValue[sentence] = freq 
+
+
+
+sumValues = 0
+for sentence in sentenceValue: 
+    sumValues += sentenceValue[sentence] 
+
+# Average value of a sentence from the original text 
+
+average = int(sumValues / len(sentenceValue)) 
+
+# Storing sentences into our summary. 
+summary = '' 
+for sentence in sentences: 
+    if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)): 
+        summary += " " + sentence 
+print(summary) 
